43fd6333a62efe44b3b71ca9156140bf9c1457d6
Anand6262 <pyexpert62@gmail.com>
2025-03-13 21:45:06 +0530

    chnages

--- src/services/PutApi.js
+++ src/services/PutApi.js
@@ -4,6 +4,13 @@ let URL = `https://jsonplaceholder.typicode.com`
 
 
 export async function putApi(queryParam) {
+    // queryParam is a json body/ json object!
+    // {
+    //     "id": 1,
+    //     "title": "Rohit Sharma",
+    //     "body": "Dummy Data",
+    //     "userId": 1
+    // }
     // console.log('queryParam :', queryParam);
 
     return await axiosInstance.put(`${URL}/posts/${queryParam.id}`, queryParam);
