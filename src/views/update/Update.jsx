a284e58ac327a9efb84a2b3566ec91558c68a0cf
Anand6262 <pyexpert62@gmail.com>
2025-03-13 20:04:22 +0530

    toast

--- src/views/update/Update.jsx
+++ src/views/update/Update.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { putApi } from '../../services/PutApi';
 import { patchApi } from '../../services/PatchApi';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Update() {
   const initialPutValues = {
@@ -21,26 +23,32 @@ function Update() {
     const { id, title, body, userId } = values;
     try {
       const response = await putApi({ id, title, body, userId });
+      toast.success('Data updated (put api) successfully!', { position: 'top-right', autoClose: 3000 });
       // console.log('response', response?.data);
       alert(JSON.stringify(response?.data))
     } catch (error) {
+      toast.error('Error updating data (put api)!', { position: 'top-right', autoClose: 3000 });
       console.error('Error updating data:', error);
     }
   }
 
-const handlePatchSubmit = async (values) => {
+  const handlePatchSubmit = async (values) => {
     const { userId, title } = values;
     try {
       const response = await patchApi({ userId, title });
       // console.log('response', response?.data);
+      toast.success('Data updated (patch api) successfully!', { position: 'top-right', autoClose: 3000 });
       alert(JSON.stringify(response?.data))
     } catch (error) {
       console.error('Error updating data:', error);
+      toast.error('Error updating data (patch api)!', { position: 'top-right', autoClose: 3000 });
     }
   }
 
   return (
     <div className="min-h-screen bg-gray-100">
+      {/* Toast Container */}
+      <ToastContainer />
       <header className="bg-gradient-to-br from-blue-100 to-indigo-100 shadow-md p-4 sticky top-0 z-50">
         <nav className="container mx-auto flex justify-between items-center">
           <Link to={'/'}>
@@ -145,7 +153,7 @@ const handlePatchSubmit = async (values) => {
           <div className="bg-white rounded-lg shadow p-6">
             <h4 className="text-xl font-semibold mb-4 text-gray-700">PATCH API</h4>
             <p className='mt-3 mb-2'>1) Very Fast</p>
-            <p className='mb-2'>2) We only send the <span className='font-bold'>field</span> with the <span className='font-bold'>ubique attribute</span> to update</p>
+            <p className='mb-2'>2) We only send the <span className='font-bold'>field</span> with the <span className='font-bold'>unique attribute</span> to update</p>
             <hr className='mb-5' />
             <Formik initialValues={initialPatchValues} onSubmit={handlePatchSubmit}>
               <Form className="space-y-4">
